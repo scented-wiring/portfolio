e8efe8c67baef56c0447c09c4dec6da6e50107f3
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-26 17:05:43 +0000

    add links to link icons

--- src/App.js
+++ src/App.js
@@ -15,28 +15,52 @@ const App = () => {
         <div id="title">Tom Hammersley - Portfolio</div>
         <div id="links">
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <div className="link">
-              <i class="fab fa-linkedin fa-3x"></i>
-              LinkedIn
-            </div>
+            <a
+              href="https://www.linkedin.com/in/tom-hammersley-3b0b4b203/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="link">
+                <i class="fab fa-linkedin fa-3x"></i>
+                LinkedIn
+              </div>
+            </a>
           </motion.div>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <div className="link">
-              <i class="fab fa-twitter-square fa-3x"></i>
-              Twitter
-            </div>
+            <a
+              href="https://twitter.com/scentedwiring"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="link">
+                <i class="fab fa-twitter-square fa-3x"></i>
+                Twitter
+              </div>
+            </a>
           </motion.div>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <div className="link">
-              <i class="fab fa-github-square fa-3x"></i>
-              Github
-            </div>
+            <a
+              href="https://github.com/scented-wiring"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="link">
+                <i class="fab fa-github-square fa-3x"></i>
+                Github
+              </div>
+            </a>
           </motion.div>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <div className="link">
-              <i class="fas fa-envelope-square fa-3x"></i>
-              Email
-            </div>
+            <a
+              href="mailto:thomas.j.hammersley@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="link">
+                <i class="fas fa-envelope-square fa-3x"></i>
+                Email
+              </div>
+            </a>
           </motion.div>
         </div>
       </div>
