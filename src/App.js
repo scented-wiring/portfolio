e1e0a19d0e11b887286845af8d7d6297d0cb0243
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-26 16:35:21 +0000

    install framer-motion, add animation and icons to links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import { motion } from "framer-motion";
 import "./App.css";
 
 const App = () => {
   return (
     <div id="App">
+      <motion.header
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 1 },
+        }}
+        whileTap={{ scale: 0.9 }}
+      />
       <div id="header">
         <div id="title">Tom Hammersley - Portfolio</div>
         <div id="links">
-          <div className="link">LinkedIn</div>
-          <div className="link">Twitter</div>
-          <div className="link">Github</div>
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="link">
+              <i class="fab fa-linkedin fa-3x"></i>
+              LinkedIn
+            </div>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="link">
+              <i class="fab fa-twitter-square fa-3x"></i>
+              Twitter
+            </div>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="link">
+              <i class="fab fa-github-square fa-3x"></i>
+              Github
+            </div>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <div className="link">
+              <i class="fas fa-envelope-square fa-3x"></i>
+              Email
+            </div>
+          </motion.div>
         </div>
       </div>
     </div>
