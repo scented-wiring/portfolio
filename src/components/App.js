08216842e3e7e0bef3154739e0f2b5702e2d4eb1
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-01-27 18:11:53 +0000

    restructure App to single page, add jump to section

--- src/components/App.js
+++ src/components/App.js
@@ -1,104 +1,11 @@
-import { Link, Switch, Route } from "react-router-dom";
-import { motion } from "framer-motion";
-import About from "./About";
-import "../styles/App.css";
+import Header from "../components/Header";
+import About from "../components/About";
 
 const App = () => {
   return (
-    <div id="App">
-      <motion.header
-        whileHover={{
-          scale: 1.2,
-          transition: { duration: 1 },
-        }}
-        whileTap={{ scale: 0.9 }}
-      />
-      <div id="header">
-        <motion.div
-          initial={{ x: 800 }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", duration: 1 }}
-        >
-          <div id="title">Tom Hammersley - Portfolio</div>
-        </motion.div>
-        <motion.div
-          initial={{ x: -700 }}
-          animate={{ x: 700 }}
-          transition={{ duration: 1 }}
-        >
-          <div id="links">
-            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
-              <a
-                href="https://www.linkedin.com/in/tom-hammersley-3b0b4b203/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="link">
-                  <i class="fab fa-linkedin fa-3x"></i>
-                  LinkedIn
-                </div>
-              </a>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
-              <a
-                href="https://twitter.com/scentedwiring"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="link">
-                  <i class="fab fa-twitter-square fa-3x"></i>
-                  Twitter
-                </div>
-              </a>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
-              <a
-                href="https://github.com/scented-wiring"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="link">
-                  <i class="fab fa-github-square fa-3x"></i>
-                  Github
-                </div>
-              </a>
-            </motion.div>
-            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
-              <a
-                href="mailto:thomas.j.hammersley@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="link">
-                  <i class="fas fa-envelope-square fa-3x"></i>
-                  Email
-                </div>
-              </a>
-            </motion.div>
-          </div>
-        </motion.div>
-      </div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <div id="navbar">
-          <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
-            <Link to="/">
-              <div className="nav">About</div>
-            </Link>
-          </motion.div>
-          <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
-            <Link to="/projects">
-              <div className="nav">Projects</div>
-            </Link>
-          </motion.div>
-        </div>
-      </motion.div>
-      <Switch>
-        <Route exact path="/" component={About} />
-      </Switch>
+    <div>
+      <Header />
+      <About />
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,107 @@
+import { motion } from "framer-motion";
+import { HashLink } from "react-router-hash-link";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <div id="Header">
+      <motion.header
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 1 },
+        }}
+        whileTap={{ scale: 0.9 }}
+      />
+      <div id="top">
+        <motion.div
+          initial={{ x: 800 }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", duration: 1 }}
+        >
+          <div id="title">Tom Hammersley - Portfolio</div>
+        </motion.div>
+        <motion.div
+          initial={{ x: -700 }}
+          animate={{ x: 700 }}
+          transition={{ duration: 1 }}
+        >
+          <div id="links">
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
+              <a
+                href="https://www.linkedin.com/in/tom-hammersley-3b0b4b203/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="link">
+                  <i class="fab fa-linkedin fa-3x"></i>
+                  LinkedIn
+                </div>
+              </a>
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
+              <a
+                href="https://twitter.com/scentedwiring"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="link">
+                  <i class="fab fa-twitter-square fa-3x"></i>
+                  Twitter
+                </div>
+              </a>
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
+              <a
+                href="https://github.com/scented-wiring"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="link">
+                  <i class="fab fa-github-square fa-3x"></i>
+                  Github
+                </div>
+              </a>
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
+              <a
+                href="mailto:thomas.j.hammersley@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="link">
+                  <i class="fas fa-envelope-square fa-3x"></i>
+                  Email
+                </div>
+              </a>
+            </motion.div>
+          </div>
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div id="navbar">
+          <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
+            <HashLink
+              smooth
+              to="#About"
+              scroll={(el) => {
+                el.scrollIntoView(true);
+                window.scrollBy(0, -150);
+              }}
+            >
+              <div className="nav">About</div>
+            </HashLink>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.9 }}>
+            <div className="nav">Projects</div>
+          </motion.div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Header;
