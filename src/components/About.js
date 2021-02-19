44a15e2af1fa801281dbd5f6fb0adb1b88a09b33
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-02-19 11:32:10 +0000

    load fonts via link, change markup

--- src/components/About.js
+++ src/components/About.js
@@ -10,68 +10,72 @@ const About = () => {
           <h1>About Me</h1>
           <img id="tom" src={tom} alt="Tom Hammersley" />
         </div>
-        <p>
-          Hi, I'm Tom. In the spring of 2020 I decided to start on a path to a
-          new career in coding by enrolling on a software engineering course at
-          the online school Manchester Codes. Since then I have dedicated all of
-          my free time to this pursuit, developing applications, reading
-          documentation, following tutorials and carefully going over course
-          materials to ensure I fully understand key concepts.
-          <br />
-          <br />I am pleased to say that in January 2021 I graduated from my
-          course and I now have experience across the full-stack and the skills
-          I need to take on a junior development role.
-          <br />
-          <br />
-          While seeking new career opportunities, I am continuing to develop
-          projects to expand on my knowledge and learn new technologies.
-        </p>
+        <article>
+          <p>
+            Hi, I'm Tom. In the spring of 2020 I decided to start on a path to a
+            new career in coding by enrolling on a software engineering course
+            at the online school Manchester Codes. Since then I have dedicated
+            all of my free time to this pursuit, developing applications,
+            reading documentation, following tutorials and carefully going over
+            course materials to ensure I fully understand key concepts.
+            <br />
+            <br />I am pleased to say that in January 2021 I graduated from my
+            course and I now have experience across the full-stack and the
+            skills I need to take on a junior development role.
+            <br />
+            <br />
+            While seeking new career opportunities, I am continuing to develop
+            projects to expand on my knowledge and learn new technologies.
+          </p>
+        </article>
         <div className="heading">
           <h2>Technologies</h2>
           <h3>Proficient in:</h3>
-          <FadeInWhenVisible>
-            <div id="technologies">
-              <div className="technology">
-                <i className="fab fa-js-square fa-3x"></i>Javascript
-              </div>
-              <div className="technology">
-                <i className="fab fa-react fa-3x"></i>React
-              </div>
-              <div className="technology">
-                <i className="fas fa-satellite-dish fa-3x"></i>express.js
-              </div>
-              <div className="technology">
-                <i className="fas fa-database fa-3x"></i>SQL
-              </div>
-              <div className="technology">
-                <i className="fab fa-node fa-3x"></i>Node.js
-              </div>
-              <div className="technology">
-                <i className="far fa-file-code fa-3x"></i>HTML
-              </div>
-              <div className="technology">
-                <i className="fas fa-paint-brush fa-3x"></i>CSS
-              </div>
-              <div className="technology">
-                <i className="fas fa-check-circle fa-3x"></i>Jest
-              </div>
-              <div className="technology">
-                <i className="fas fa-check-circle fa-3x"></i>Enzyme
-              </div>
-            </div>
-          </FadeInWhenVisible>
-          <h3>Currently learning:</h3>
-          <FadeInWhenVisible>
-            <div id="technologies">
-              <div className="technology">
-                <i className="fas fa-keyboard fa-3x"></i>Typescript
-              </div>
-              <div className="technology">
-                <i className="fab fa-vuejs fa-3x"></i>Vue
-              </div>
-            </div>
-          </FadeInWhenVisible>
         </div>
+        <FadeInWhenVisible>
+          <div id="technologies">
+            <div className="technology">
+              <i className="fab fa-js-square fa-3x"></i>Javascript
+            </div>
+            <div className="technology">
+              <i className="fab fa-react fa-3x"></i>React
+            </div>
+            <div className="technology">
+              <i className="fas fa-satellite-dish fa-3x"></i>express.js
+            </div>
+            <div className="technology">
+              <i className="fas fa-database fa-3x"></i>SQL
+            </div>
+            <div className="technology">
+              <i className="fab fa-node fa-3x"></i>Node.js
+            </div>
+            <div className="technology">
+              <i className="far fa-file-code fa-3x"></i>HTML
+            </div>
+            <div className="technology">
+              <i className="fas fa-paint-brush fa-3x"></i>CSS
+            </div>
+            <div className="technology">
+              <i className="fas fa-check-circle fa-3x"></i>Jest
+            </div>
+            <div className="technology">
+              <i className="fas fa-check-circle fa-3x"></i>Enzyme
+            </div>
+          </div>
+        </FadeInWhenVisible>
+        <div className="heading">
+          <h3>Currently learning:</h3>
+        </div>
+        <FadeInWhenVisible>
+          <div id="technologies">
+            <div className="technology">
+              <i className="fas fa-keyboard fa-3x"></i>Typescript
+            </div>
+            <div className="technology">
+              <i className="fab fa-vuejs fa-3x"></i>Vue
+            </div>
+          </div>
+        </FadeInWhenVisible>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,6 +21,8 @@ const Projects = ({ beautify }) => {
       <div className="box">
         <div className="heading">
           <h1>Projects</h1>
+        </div>
+        <article>
           <p>
             Below is a list of some of the projects I've completed since I
             started training to be a developer.
@@ -33,53 +35,50 @@ const Projects = ({ beautify }) => {
             Note: Heroku apps sometimes take a minute to load so please be
             patient <i className="far fa-smile-beam"></i>
           </div>
+        </article>
 
-          <div id="project-gallery">
-            {projectsArray.map((project) => {
-              return (
-                <SlideWhenVisible key={projectsArray.indexOf(project)}>
-                  <div
-                    className="project"
-                    onMouseOver={() => setHover(project.name)}
-                    onMouseOut={() => setHover("")}
-                    key={projectsArray.indexOf(project)}
-                  >
-                    <img
-                      src={project.image}
-                      alt={`${project.name} screenshot`}
-                    />
+        <div id="project-gallery">
+          {projectsArray.map((project) => {
+            return (
+              <SlideWhenVisible key={projectsArray.indexOf(project)}>
+                <div
+                  className="project"
+                  onMouseOver={() => setHover(project.name)}
+                  onMouseOut={() => setHover("")}
+                  key={projectsArray.indexOf(project)}
+                >
+                  <img src={project.image} alt={`${project.name} screenshot`} />
 
-                    <div className={setClassName(project.name)}>
-                      <div className="project-title">{project.name}</div>
-                      <div className="project-links">
-                        {project.links.map((link) => {
-                          return (
-                            <a
-                              href={link.href}
-                              target="_blank"
-                              rel="noreferrer"
-                              key={project.links.indexOf(link)}
-                            >
-                              <AnimateLink key={project.links.indexOf(link)}>
-                                <i className="fas fa-link"></i>
-                                {link.destination}
-                              </AnimateLink>
-                            </a>
-                          );
-                        })}
-                      </div>
-                      <div className="project-description">
-                        {project.description}
-                      </div>
-                      <div className="project-technologies">
-                        {project.technologies}
-                      </div>
+                  <div className={setClassName(project.name)}>
+                    <div className="project-title">{project.name}</div>
+                    <div className="project-links">
+                      {project.links.map((link) => {
+                        return (
+                          <a
+                            href={link.href}
+                            target="_blank"
+                            rel="noreferrer"
+                            key={project.links.indexOf(link)}
+                          >
+                            <AnimateLink key={project.links.indexOf(link)}>
+                              <i className="fas fa-link"></i>
+                              {link.destination}
+                            </AnimateLink>
+                          </a>
+                        );
+                      })}
+                    </div>
+                    <div className="project-description">
+                      {project.description}
+                    </div>
+                    <div className="project-technologies">
+                      {project.technologies}
                     </div>
                   </div>
-                </SlideWhenVisible>
-              );
-            })}
-          </div>
+                </div>
+              </SlideWhenVisible>
+            );
+          })}
         </div>
       </div>
     </div>
